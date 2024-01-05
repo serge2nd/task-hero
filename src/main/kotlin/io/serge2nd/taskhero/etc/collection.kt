b5f2c2786892fdbb3f76c1cf24cdbbda0c12c344
Sergey Pistyulga <serge2nd@yandex.ru>
2024-01-05 12:07:10 +0400

    Static memory in durationSum()

--- src/main/kotlin/io/serge2nd/taskhero/etc/collection.kt
+++ src/main/kotlin/io/serge2nd/taskhero/etc/collection.kt
@@ -1,5 +1,17 @@
 package io.serge2nd.taskhero.etc
 
 import java.time.Duration
+import java.time.Duration.ofSeconds
 
-fun Iterable<Duration>.durationSum(): Duration = fold(Duration.ZERO, Duration::plus)
+fun Iterable<Duration>.durationSum() = fold(TimeAmount(), ::addTo).let(::duration)
+
+private data class TimeAmount(var secs: Long = 0, var nanos: Int = 0)
+
+private fun addTo(t: TimeAmount, d: Duration) = t.apply {
+    secs += d.toSeconds() + (nanos + d.toNanosPart()) / SEC_NANOS
+    nanos = (nanos + d.toNanosPart()) % SEC_NANOS
+}
+
+private fun duration(t: TimeAmount): Duration = ofSeconds(t.secs, t.nanos.toLong())
+
+private const val SEC_NANOS = 1_000_000_000
